5edf3d26ba8927d0635f5282b3b697a224ac34cf
Rahul Kumar <97732927+rahul-kumar-bly@users.noreply.github.com>
2022-04-12 16:53:48 +0530

    Update goodread.py

--- goodread.py
+++ goodread.py
@@ -1,6 +1,4 @@
 # import libraries
-from tkinter import BROWSE
-from tkinter.messagebox import RETRY
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
